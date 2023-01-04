dba29f7b328cf57d9cb0b2c89e9e75cc5c84ad25
roseInWeb <i.a.semyonova@mail.ru>
2023-01-04 21:29:06 +0400

    fix: the task is done!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  if (n !== 0) {
+    let str = `${n}`;
+    let arr = str.split('');
+    let arrRev = arr.reverse();
+    str = arrRev.join('');
+    return parseInt(str);
+  } else {
+    return 0;
+  }
 }
